422ef63b8c42f5b6d57cd20a2bf0ab71bf6b9334
Lucas Entreportes <lucasentreportes@hotmail.com>
2022-11-20 01:56:04 -0300

    takeover server call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -123,15 +123,15 @@ export const getServerSideProps = async () => {
   let guessCount
   let userCount
   try{
-    console.log(api.request)
-    const [poolCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
-      api.get('pools/count'),
-      api.get('guesses/count'),
-      api.get('users/count')
-    ])
-    poolCount = poolCountResponse.data.count
-    guessCount = guessCountResponse.data.count
-    userCount = userCountResponse.data.count
+    // console.log(api.request)
+    // const [poolCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
+    //   api.get('pools/count'),
+    //   api.get('guesses/count'),
+    //   api.get('users/count')
+    // ])
+    // poolCount = poolCountResponse.data.count
+    // guessCount = guessCountResponse.data.count
+    // userCount = userCountResponse.data.count
       
     
     
