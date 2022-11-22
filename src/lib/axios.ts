e768061ad77ad5d38562588fe81759b76d2e881c
Lucas Entreportes <lucasentreportes@hotmail.com>
2022-11-21 21:44:48 -0300

    adding serverRenato

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://192.168.0.13:3333',
-    timeout: 500,
+    //baseURL: 'http://192.168.0.13:3333',
+    baseURL: 'https://bolao-copa.mhsw.com.br/',
+    timeout: 600,
 })
